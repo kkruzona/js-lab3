7c4c186cb5243a5bd7b0ab0f9c22291cefc1f78a
Kelsey Kruzona <kelsey.herlein@gmail.com>
2021-04-01 22:12:00 -0400

    made it to step 6

--- script.js
+++ script.js
@@ -33,10 +33,121 @@ for(let submission of submissions){
         Passed: ${submission.passed}`)
 }
 
-function addSubmission(array , newName , newScore , newDate){
+function addSubmission(array, newName, newScore, newDate) {
+        let newPerson = {
+            name: newName,
+            score: newScore,
+            date: newDate,
+            passed: true
+        }
 
-}
+        array.push(newPerson);
+        console.log(submissions);
+        if (newPerson.score >= 60){
+            newPerson.passed = true;
+        }
+        else{
+            newPerson.passed = false;
+        }
+
+    }
+    addSubmission(submissions, 'Phil', 65, '2020-01-22');
+
+
+    function deleteSubmissionByIndex(array, index) {
+        array.splice(index, 1);
+    }
+    deleteSubmissionByIndex(submissions, 1);
+    console.log(submissions);
+
+
+    function deleteSubmissionByName(array, name) {
+        const index = array.findIndex(submission => submission.name === name);
+        array.splice(index, 1);
+
+    }
+    deleteSubmissionByName(submissions, 'Jack');
+    console.log(submissions);
+
+
+    function editSubmission(array , index , score) {
+        array[index].score = score;
+        if (array[index].score >= 60){
+            array[index].passed = true;
+        }
+        else{
+            array[index].passed = false;
+        }
+    }
+    editSubmission(submissions, 1, 50);
+    console.log(submissions);
+
+    function findSubmissionByName(array , name) {
+        const result = array.find(submission => submission.name === name);
+    }
+    findSubmissionByName(submissions, 'Jill');
+    console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // addSubmission(newPerson);
+    // addSubmission = ('Phil', 60, '2020-01-22')
+
+
+// function addSubmission(array, newName , newScore , newDate){
+//     let newPerson =[
+//         {
+//             name: newName,
+//             score: newScore,
+//             date: newDate
+//         }
+//     ];
+//     submissions = submissions.push(newPerson);
+//     console.log(submissions);
+// }
+// addSubmission('Phil', 60, '2020-01-22');
+
+// if (this.passed.value >= 60){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
 
 // function deleteSubmissionByIndex(array , index){
     
+// }
+
+
+// function addSubmission(array, newName , newScore , newDate){
+//     let newPerson =[
+//         {
+//             name: 'Phil',
+//             score: 60,
+//             date: '2020-01-22',
+//         }
+//     ];
+//     submissions = submissions.push(newPerson);
+//     console.log(submissions);
+//     if (submissions.passed >= 60){
+//     return true;
+//     }
+//     else{
+//     return false;
+//     }
 // }
